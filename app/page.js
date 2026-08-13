ecde5d5927e84889cfd42a4e1518909b09494dbe
Sidhant575 <goyalsidhant575@gmail.com>
2026-08-13 18:51:58 +0530

    Finishing touches

--- app/page.js
+++ app/page.js
@@ -1,38 +1,4 @@
-// import Image from "next/image";
-// import localFont from "next/font/local"
-// import Link from "next/link";
 
-// const poppins = localFont({
-//   src: "./fonts/Poppins-ExtraBold.ttf",
-//   variable: "--font-poppins",
-//   subsets: ["latin"],
-// });
-
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <section className="grid grid-cols-2 h-[50vh] ">
-//         <div className="flex flex-col gap-4 justify-center items-center" >
-//           <p className={`text-3xl font-bold ${poppins.className}`}>
-//             The best URL Shortner in the Market
-//           </p>
-//           <p className="px-30 text-center">
-//             We are the most straight forward URL shortner in the world. Most of the url shortners will track you or ask you to give your details for login. We understand your needs and hence we created this URL hortner
-//           </p>
-//           <div className='flex gap-3'>
-//             <Link href='/generate'><button className='bg-gray-900 rounded-lg shadow-lg p-3 lg py-1 font-bold text-white'>Try Now</button></Link>
-//             <Link href='/github'><button className='bg-gray-900 rounded-lg shadow-lg p-3 lg py-1 font-bold text-white'>GitHub</button></Link>
-
-//           </div>
-//         </div>
-//         <div className=" flex justify-start relative">
-//           <Image className="py-3" src={"/vector.svg"} alt="an image of a vector" fill={true} />
-//         </div>
-//       </section>
-//     </main>
-//   );
-// }
 import Image from "next/image";
 import localFont from "next/font/local";
 import Link from "next/link";
@@ -54,14 +20,14 @@ export default function Home() {
           <p
             className={`text-2xl sm:text-3xl md:text-4xl font-bold text-center ${poppins.className}`}
           >
-            The best URL Shortner in the Market
+            The best URL Shortener in the Market
           </p>
 
           <p className="text-sm sm:text-base md:text-lg text-center max-w-xl">
-            We are the most straight forward URL shortner in the world.
-            Most of the url shortners will track you or ask you to give your
+            We are the most straight forward URL shortener in the world.
+            Most of the url shorteners will track you or ask you to give your
             details for login. We understand your needs and hence we created
-            this URL shortner.
+            this URL shortener.
           </p>
 
           <div className="flex flex-col sm:flex-row gap-3">
